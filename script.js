5adbb38808e330feb35faf4bb323ce2b03211927
anakarinabr <ana.blanco@correo.unimet.edu.ve>
2024-02-11 13:21:48 -0400

    Puntajes listos, falta local storage

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let tableros = [];
+let puntos_player1 = 0;
+let puntos_player2 = 0;
+let puntos_player3 = 0;
+let puntos_player4 = 0;
 
 //CONTADOR TURNOS
 var countRounds = 0;
@@ -11,13 +15,15 @@ const randomUniqueNumbers = getRandomUniqueNumbers(min, max, quantity);
 var arrayindex = 0;
 
 function generateBoard(size) {
+  tableros = [];
+  countRounds = 0;
+  arrayindex = 0;
   DeleteDashboard();
 
   mostrarFlex("Tablas");
 
   const boardContainer = document.getElementById("board");
   boardContainer.innerHTML = "";
- 
 
   for (let j = 0; j < 4; j++) {
     const contenedor = document.createElement("div");
@@ -48,12 +54,9 @@ function generateBoard(size) {
       contenedor.appendChild(name);
       contenedor.id = "contenedor_jugador4";
     }
-    console.log(contenedor);
 
-   
     let tableroarray = [];
     const tablero = document.createElement("div");
-    
 
     if (size === 3) {
       tablero.classList.add("table3");
@@ -62,14 +65,17 @@ function generateBoard(size) {
     } else {
       tablero.classList.add("table5");
     }
-
-    for (let i = 0; i < size * size; i++) {
-      const cell = document.createElement("div");
-      cell.classList.add("board-cell");
-      const num = getRandomNumber();
-      cell.textContent = num;
-      tableroarray.push(num);
-      tablero.appendChild(cell);
+    for (let i = 0; i < size; i++) {
+      const columnas = [];
+      for (let i = 0; i < size; i++) {
+        const cell = document.createElement("div");
+        cell.classList.add("board-cell");
+        const num = getRandomNumber();
+        columnas.push(num);
+        cell.textContent = num;
+        tablero.appendChild(cell);
+      }
+      tableroarray.push(columnas);
     }
 
     contenedor.appendChild(tablero);
@@ -84,15 +90,31 @@ function getRandomNumber() {
 
 function number() {
   countRounds++;
-  let num = randomUniqueNumbers[arrayindex];
-  arrayindex++;
-  const element = document.getElementById("number_aleatorio");
-  element.innerHTML = num;
-  changeCell(num);
+  const tablero1 = tableros[0];
+  const tablero2 = tableros[1];
+  const tablero3 = tableros[2];
+  const tablero4 = tableros[3];
+
+  if (
+    countRounds < 25 &&
+    !verificarCartonLleno(tablero1) &&
+    !verificarCartonLleno(tablero2) &&
+    !verificarCartonLleno(tablero3) &&
+    !verificarCartonLleno(tablero4)
+  ) {
+    let num = randomUniqueNumbers[arrayindex];
+    arrayindex++;
+    const element = document.getElementById("number_aleatorio");
+    element.innerHTML = num;
+    changeCell(num);
+  } else {
+    verificarPuntos();    
+  }
 }
 
 function changeCell(aleatorio) {
   const cell = document.querySelectorAll(".board-cell");
+
   cell.forEach((cell) => {
     let number = cell.innerHTML;
 
@@ -100,6 +122,39 @@ function changeCell(aleatorio) {
       cell.style.backgroundColor = "red";
     }
   });
+
+  for (let i = 0; i < tableros.length; i++) {
+    //Ahora cambiamos el valor dentro de la matriz
+
+    if (i == 0) {
+      const posicion = encontrarPosicion(tableros[0], aleatorio);
+
+      if (posicion != -1) {
+        tableros[0][posicion["fila"]][posicion["columna"]] = "x";
+
+      }
+    } else if (i == 1) {
+      const posicion = encontrarPosicion(tableros[1], aleatorio);
+
+      if (posicion != -1) {
+        tableros[1][posicion["fila"]][posicion["columna"]] = "x";
+
+      }
+    } else if (i == 2) {
+      const posicion = encontrarPosicion(tableros[2], aleatorio);
+
+      if (posicion != -1) {
+        tableros[2][posicion["fila"]][posicion["columna"]] = "x";
+
+      }
+    } else {
+      const posicion = encontrarPosicion(tableros[3], aleatorio);
+
+      if (posicion != -1) {
+        tableros[3][posicion["fila"]][posicion["columna"]] = "x";
+      }
+    }
+  }
 }
 
 function DeleteDashboard() {
@@ -134,64 +189,272 @@ function getRandomUniqueNumbers(min, max, quantity) {
   return randomNumbers;
 }
 
-function ocultarTablas(jugador){
-    
-    if(jugador.id == "jugador1"){
-        resetearValores();
-        const jugador_tablero = document.getElementById("contenedor_jugador2");
-        jugador_tablero.style.display="none";
+function ocultarTablas(jugador) {
+  if (jugador.id == "jugador1") {
+    resetearValores();
+    const jugador_tablero = document.getElementById("contenedor_jugador2");
+    jugador_tablero.style.display = "none";
 
-        const jugador_tablero3 = document.getElementById("contenedor_jugador3");
-        jugador_tablero3.style.display="none";
+    const jugador_tablero3 = document.getElementById("contenedor_jugador3");
+    jugador_tablero3.style.display = "none";
 
-        const jugador_tablero4 = document.getElementById("contenedor_jugador4");
-        jugador_tablero4.style.display="none";
-    }else if(jugador.id == "jugador2"){
+    const jugador_tablero4 = document.getElementById("contenedor_jugador4");
+    jugador_tablero4.style.display = "none";
+  } else if (jugador.id == "jugador2") {
+    resetearValores();
 
-       resetearValores();
+    const jugador_tablero1 = document.getElementById("contenedor_jugador1");
+    jugador_tablero1.style.display = "none";
 
-        const jugador_tablero1 = document.getElementById("contenedor_jugador1");
-        jugador_tablero1.style.display="none";
+    const jugador_tablero3 = document.getElementById("contenedor_jugador3");
+    jugador_tablero3.style.display = "none";
 
-        const jugador_tablero3 = document.getElementById("contenedor_jugador3");
-        jugador_tablero3.style.display="none";
+    const jugador_tablero4 = document.getElementById("contenedor_jugador4");
+    jugador_tablero4.style.display = "none";
+  } else if (jugador.id == "jugador3") {
+    resetearValores();
+    const jugador_tablero = document.getElementById("contenedor_jugador2");
+    jugador_tablero.style.display = "none";
 
-        const jugador_tablero4 = document.getElementById("contenedor_jugador4");
-        jugador_tablero4.style.display="none";
-    }else if(jugador.id == "jugador3"){
-        resetearValores();
-        const jugador_tablero = document.getElementById("contenedor_jugador2");
-        jugador_tablero.style.display="none";
+    const jugador_tablero1 = document.getElementById("contenedor_jugador1");
+    jugador_tablero1.style.display = "none";
 
-        const jugador_tablero1 = document.getElementById("contenedor_jugador1");
-        jugador_tablero1.style.display="none";
+    const jugador_tablero4 = document.getElementById("contenedor_jugador4");
+    jugador_tablero4.style.display = "none";
+  } else if (jugador.id == "jugador4") {
+    resetearValores();
+    const jugador_tablero = document.getElementById("contenedor_jugador2");
+    jugador_tablero.style.display = "none";
 
-        const jugador_tablero4 = document.getElementById("contenedor_jugador4");
-        jugador_tablero4.style.display="none";
-    }else if(jugador.id == "jugador4"){
-        resetearValores();
-        const jugador_tablero = document.getElementById("contenedor_jugador2");
-        jugador_tablero.style.display="none";
+    const jugador_tablero3 = document.getElementById("contenedor_jugador3");
+    jugador_tablero3.style.display = "none";
 
-        const jugador_tablero3 = document.getElementById("contenedor_jugador3");
-        jugador_tablero3.style.display="none";
-
-        const jugador_tablero4 = document.getElementById("contenedor_jugador1");
-        jugador_tablero4.style.display="none";
-    }
+    const jugador_tablero4 = document.getElementById("contenedor_jugador1");
+    jugador_tablero4.style.display = "none";
+  }
 }
 
-function resetearValores(){
-    const jugador_tablero = document.getElementById("contenedor_jugador2");
-        jugador_tablero.style.display="flex";
+function resetearValores() {
+  const jugador_tablero = document.getElementById("contenedor_jugador2");
+  jugador_tablero.style.display = "flex";
+  jugador_tablero.style.flexDirection = "column";
 
-        const jugador_tablero3 = document.getElementById("contenedor_jugador3");
-        jugador_tablero3.style.display="flex";
+  const jugador_tablero3 = document.getElementById("contenedor_jugador3");
+  jugador_tablero3.style.display = "flex";
+  jugador_tablero3.style.flexDirection = "column";
 
-        const jugador_tablero4 = document.getElementById("contenedor_jugador4");
-        jugador_tablero4.style.display="flex";
+  const jugador_tablero4 = document.getElementById("contenedor_jugador4");
+  jugador_tablero4.style.display = "flex";
+  jugador_tablero4.style.flexDirection = "column";
 
-        const jugador_tablero1 = document.getElementById("contenedor_jugador1");
-        jugador_tablero1.style.display="flex";
+  const jugador_tablero1 = document.getElementById("contenedor_jugador1");
+  jugador_tablero1.style.display = "flex";
+  jugador_tablero1.style.flexDirection = "column";
+}
+
+function showDashboard() {
+  countRounds = 0;
+  const element_new = document.getElementById("Tablas");
+  element_new.style.display = "none";
+
+  const element = document.getElementById("form");
+  element.style.display = "flex";
+
+  element.style.flexDirection = "column";
+  element.style.justifyContent = "center";
+}
+
+function verificarCartonLleno(matriz) {
+  const n = matriz.length;
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (matriz[i][j] !== "x") {
+        return false; // Se encontró un valor diferente de 'x'
+      }
+    }
+  }
+
+  return true; // Todos los valores son 'x'
+}
+
+function encontrarPosicion(matriz, valor) {
+  for (let i = 0; i < matriz.length; i++) {
+    for (let j = 0; j < matriz[i].length; j++) {
+      if (matriz[i][j] == valor) {
+        return { fila: i, columna: j };
+      }
+    }
+  }
+
+  return -1; // Devuelve -1 si no se encuentra el valor en la matriz
+}
+
+function verificarDiagonalPrincipal(matriz) {
+  const n = matriz.length;
+
+  // Verificar la diagonal principal
+  for (let i = 0; i < n; i++) {
+    if (matriz[i][i] !== "x") {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function verificarDiagonalSecundaria(matriz) {
+  const n = matriz.length;
+  // Verificar la diagonal secundaria
+  for (let i = 0; i < n; i++) {
+    if (matriz[i][n - 1 - i] !== "x") {
+      return false;
+    }
+  }
+  return true;
+}
+
+function filasConX(matriz) {
+  let contador = 0;
+
+  for (let i = 0; i < matriz.length; i++) {
+    let filaConX = true;
+
+    for (let j = 0; j < matriz[i].length; j++) {
+      if (matriz[i][j] !== "x") {
+        filaConX = false;
+        break;
+      }
+    }
+
+    if (filaConX) {
+      contador++;
+    }
+  }
+
+  return contador;
+}
+
+function columnasConX(matriz) {
+  let contador = 0;
+
+  for (let j = 0; j < matriz[0].length; j++) {
+    let columnaConX = true;
+
+    for (let i = 0; i < matriz.length; i++) {
+      if (matriz[i][j] !== "x") {
+        columnaConX = false;
+        break;
+      }
+    }
+
+    if (columnaConX) {
+      contador++;
+    }
+  }
+
+  return contador;
+}
+
+function verificarPuntos(){
+
+    const element = document.getElementById("Tablas");
+    element.style.display = "none";
+
+    const element1 = document.getElementById("puntajes");
+    element1.style.display = "flex";
+
+    for(let i=0 ; i<4 ; i++){
+
+        if(i==0){
+            	const lleno = verificarCartonLleno(tableros[0]);
+                const principal = verificarDiagonalPrincipal(tableros[0]);
+                const secundaria = verificarDiagonalSecundaria(tableros[0]);
+                const filas = filasConX(tableros[0]);
+                const columnas = columnasConX(tableros[0]);
+                
+                if(lleno){
+                    puntos_player1 += 5;
+                } if(principal){
+                    puntos_player1 += 3;
+                }if(secundaria){
+                    puntos_player1 += 3;
+                }if(filas != 0 || columnas != 0){
+                    puntos_player1 += filas+columnas;
+                }
+
+                const jugadorpuntos= document.createElement('li');
+                jugadorpuntos.innerHTML = document.getElementById("jugador1").value+ ": "+ puntos_player1;
+                const lista = document.getElementById("Lista");
+                lista.appendChild(jugadorpuntos);
+
+        }else if(i == 1){
+
+            const lleno = verificarCartonLleno(tableros[1]);
+                const principal = verificarDiagonalPrincipal(tableros[1]);
+                const secundaria = verificarDiagonalSecundaria(tableros[1]);
+                const filas = filasConX(tableros[1]);
+                const columnas = columnasConX(tableros[1]);
+                
+                if(lleno){
+                    puntos_player2 += 5;
+                } if(principal){
+                    puntos_player2 += 3;
+                }if(secundaria){
+                    puntos_player2 += 3;
+                }if(filas != 0 || columnas != 0){
+                    puntos_player2 += filas+columnas;
+                }
+
+                const jugadorpuntos= document.createElement('li');
+                jugadorpuntos.innerHTML = document.getElementById("jugador2").value+ ": "+ puntos_player2;
+                const lista = document.getElementById("Lista");
+                lista.appendChild(jugadorpuntos);
+        }else if(i == 2){
+
+            const lleno = verificarCartonLleno(tableros[2]);
+                const principal = verificarDiagonalPrincipal(tableros[2]);
+                const secundaria = verificarDiagonalSecundaria(tableros[2]);
+                const filas = filasConX(tableros[2]);
+                const columnas = columnasConX(tableros[2]);
+                
+                if(lleno){
+                    puntos_player3 += 5;
+                }if(principal){
+                    puntos_player3 += 3;
+                } if(secundaria){
+                    puntos_player3 += 3;
+                } if(filas != 0 || columnas != 0){
+                    puntos_player3 += filas+columnas;
+                }
+
+                const jugadorpuntos= document.createElement('li');
+                jugadorpuntos.innerHTML = document.getElementById("jugador3").value+ ": "+ puntos_player3;
+                const lista = document.getElementById("Lista");
+                lista.appendChild(jugadorpuntos);
+        }else if(i == 3){
+
+            const lleno = verificarCartonLleno(tableros[3]);
+                const principal = verificarDiagonalPrincipal(tableros[3]);
+                const secundaria = verificarDiagonalSecundaria(tableros[3]);
+                const filas = filasConX(tableros[3]);
+                const columnas = columnasConX(tableros[3]);
+                
+                if(lleno){
+                    puntos_player4 += 5;
+                }if(principal){
+                    puntos_player4 += 3;
+                } if(secundaria){
+                    puntos_player4 += 3;
+                }if(filas != 0 || columnas != 0){
+                    puntos_player4 += filas+columnas;
+                }
+
+                const jugadorpuntos= document.createElement('li');
+                jugadorpuntos.innerHTML = document.getElementById("jugador4").value+ ": "+ puntos_player4;
+                const lista = document.getElementById("Lista");
+                lista.appendChild(jugadorpuntos);
+        }
+    }
 
 }
